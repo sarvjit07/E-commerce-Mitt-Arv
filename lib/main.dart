953e6860c1efca732db0041a8f9c9f1e00917afa
sarvjit07 <sarvjitlala@gmail.com>
2025-01-24 23:04:54 +0530

    firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
@@ -7,8 +9,23 @@ import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart'; // Import the register screen
 
-void main() {
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if(kIsWeb){
+    await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyAx6h2U5JaBDD0pME3PfEWSWOEKtgs6e3E",
+  authDomain: "e-com-flutter-f817c.firebaseapp.com",
+  projectId: "e-com-flutter-f817c",
+  storageBucket: "e-com-flutter-f817c.firebasestorage.app",
+  messagingSenderId: "531008692877",
+  appId: "1:531008692877:web:389df27378d0f565a178c4",
+  measurementId: "G-N4CK84V46C"));
   runApp(const MyApp());
+  }else{
+    await Firebase.initializeApp();
+  }
+  
 }
 
 class MyApp extends StatelessWidget {
